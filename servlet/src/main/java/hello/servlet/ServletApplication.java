5420a398c5f5bcef103cde047b72af009d08bf4d
mgOh <issac_oh@naver.com>
2022-03-29 23:09:37 +0900

    comment: ServletApplication
    // 김영한 스프링 MVC 1편 - 2장 서블릿 목차 및 정리

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -12,5 +12,16 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-// 커밋 확인 - Preferences
+	// 서블릿 - 프로젝트 생성
+	// 서블릿 - Hello 서블릿 (hello.servlet.basic.HelloServlet)
+	// 서블릿 - HttpServletRequest - 개요
+	// 서블릿 - HttpServletRequest - 기본 사용법 (hello.servlet.basic.request.RequestHeaderServlet)
+	// 서블릿 - HTTP 요청 데이터 - 개요
+	// 서블릿 - HTTP 요청 데이터 - GET 쿼리 파라미터 (hello.servlet.basic.request.RequestParamServlet)
+	// 서블릿 - HTTP 요청 데이터 - POST HTML Form (src/main/webapp/basic/hello-form.html)
+	// 서블릿 - HTTP 요청 데이터 - API 메시지 바디 - 단순 텍스트 (hello.servlet.basic.request.RequestBodyStringServlet)
+	// 서블릿 - HTTP 요청 데이터 - API 메시지 바디 - JSON (hello.servlet.basic.request.RequestBodyJsonServlet / hello.servlet.basic.HelloData)
+	// 서블릿 - HttpServletResponse - 기본 사용법 (hello.servlet.basic.response.ResponseHeaderServlet)
+	// 서블릿 - HTTP 응답 데이터 - 단순 텍스트, HTML (hello.servlet.web.response.ResponseHtmlServlet)
+	// 서블릿 - HTTP 응답 데이터 - API JSON (hello.servlet.web.response. ResponseJsonServlet)
 }
