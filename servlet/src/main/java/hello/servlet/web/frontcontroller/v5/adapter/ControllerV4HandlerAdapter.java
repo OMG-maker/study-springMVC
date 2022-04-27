867df6f6c1d3cceee1c375bd77023ca008a8a619
mgOh <issac_oh@naver.com>
2022-04-27 23:47:17 +0900

    add: ControllerV4HandlerAdapter update: FrontControllerServletV5
    // 핸들러 어댑터를 통해서 여러 종류의 컨트롤러를 사용할 수 있게 됨.
    // V3 와 V4 가 process 메스드 내부에서 model 을 다루는 방법이 서로 다르기 때문에 V4HandlerAdapter 에선 mv.setModel 만 주의하면 이번 파트는 어려운 부분이 따로 없음.

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -1,7 +1,7 @@
 package hello.servlet.web.frontcontroller.v5.adapter;
 
 import hello.servlet.web.frontcontroller.ModelView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
 import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
 
 import javax.servlet.ServletException;
@@ -11,20 +11,24 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
-    // 핵심
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
     @Override
     public boolean supports(Object handler) {
-        // handler 가 ControllerV3 인지, 혹은 ControllerV3 를 상속받는 클래스인지 확인할 수 있음
-        return (handler instanceof ControllerV3);
+        // handler 가 ControllerV4 인지, 혹은 ControllerV4 를 상속받는 클래스인지 확인할 수 있음
+        return (handler instanceof ControllerV4);
     }
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
-        ControllerV3 controller = (ControllerV3) handler;
+        ControllerV4 controller = (ControllerV4) handler;
 
         Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controller.process(paramMap);
+        Map<String, Object> model = new HashMap<>();
+
+//        String viewName = controller.process(paramMap, model);
+//        ModelView mv = new ModelView(viewName);
+        ModelView mv = new ModelView(controller.process(paramMap, model));
+        mv.setModel(model);
 
         return mv;
     }
